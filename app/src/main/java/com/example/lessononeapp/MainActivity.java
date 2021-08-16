6b73dc86cab69e6d88f1317a47149235bb306e0f
myasnick-off <cyber.jokep@gmail.com>
2021-08-16 20:50:23 +0300

    homework for lesson 1

--- app/src/main/java/com/example/lessononeapp/MainActivity.java
+++ app/src/main/java/com/example/lessononeapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.lessononeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +8,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.edit_text_example);
     }
+
+    /**
+     * ems - типографский термин, единица измерения ширины текста, равная ширине латинской буквы m.
+     * Применяется в TextView и EditText для определения ширины поля для текста.
+     * Работает только в случае выбора значения layout_width "wrap_content".
+     * То есть если указать android:ems="5", то это значит задать ширину поля для текста равную ширине пяти букв m.
+     **/
 }
